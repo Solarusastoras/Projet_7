36e700654af4654b095eea323181d96e460893ff
Solarusastoras <micformweb@gmail.com>
2024-08-15 07:23:37 +0200

    Site fini

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -36,29 +36,29 @@ function Apropos() {
         alt="une coligne avec vu sur les montagnes"
       />
       <div className="alignement_vertical">
-      <ToggleSection title="Fiabilité">
-        Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-        photos sont conformes aux logements, et toutes les informations sont
-        régulièrement vérifiées par nos équipes.
-      </ToggleSection>
-      <ToggleSection title="Respect">
-        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-        comportement discriminatoire ou de perturbation du voisinage entraînera
-        une exclusion de notre plateforme.
-      </ToggleSection>
-      <ToggleSection title="Service">
-        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-        comportement discriminatoire ou de perturbation du voisinage entraînera
-        une exclusion de notre plateforme.
-      </ToggleSection>
-      <ToggleSection title="Sécurité">
-        La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour
-        les voyageurs, chaque logement correspond aux critères de sécurité
-        établis par nos services. En laissant une note aussi bien à l'hôte qu'au
-        locataire, cela permet à nos équipes de vérifier que les standards sont
-        bien respectés. Nous organisons également des ateliers sur la sécurité
-        domestique pour nos hôtes.
-      </ToggleSection>
+        <ToggleSection title="Fiabilité">
+          Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.
+        </ToggleSection>
+        <ToggleSection title="Respect">
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </ToggleSection>
+        <ToggleSection title="Service">
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </ToggleSection>
+        <ToggleSection title="Sécurité">
+          La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les
+          standards sont bien respectés. Nous organisons également des ateliers
+          sur la sécurité domestique pour nos hôtes.
+        </ToggleSection>
       </div>
     </div>
   );
